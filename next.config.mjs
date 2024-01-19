900bc8a1783788421b27dbe1c9b8c0fd7609e91e
fitodac <fitodac@gmail.com>
2024-01-19 12:53:21 -0300

    pages test 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
-	trailingSlash: false,
-	skipTrailingSlashRedirect: true,
-	distDir: 'docs',
-	assetPrefix: 'https://fitodac.github.io/http-status-codes/'
+	// trailingSlash: false,
+	// skipTrailingSlashRedirect: true,
+	// distDir: 'docs',
+	// assetPrefix: 'https://fitodac.github.io/http-status-codes'
 }
 
 export default nextConfig
