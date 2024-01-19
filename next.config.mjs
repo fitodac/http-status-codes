a9d71bfb1e9c439c77d583f8abc23e10a20b20af
fitodac <fitodac@gmail.com>
2024-01-19 13:30:36 -0300

    t3

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@
 const nextConfig = {
 	output: 'export',
 	trailingSlash: true,
-	// skipTrailingSlashRedirect: true,
-	// distDir: 'docs',
-	// assetPrefix: 'https://fitodac.github.io/http-status-codes'
 }
 
 export default nextConfig
